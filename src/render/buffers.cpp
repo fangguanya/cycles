52d0fa616384eb6ef5c15bd8af8c30fa9a1d7b98
Nathan Letwory <nathan@mcneel.com>
2016-10-31 13:09:52 +0200

    Use HALF4 buffers, so no more byte buffers for RhinoCycles.

--- src/render/buffers.cpp
+++ src/render/buffers.cpp
@@ -441,14 +441,11 @@ void DisplayBuffer::get_pixels(Device *device, float* pixels)
 	if (w == 0 || h == 0)
 		return;
 
-	if (half_float)
-		return;
-
 	/* read buffer from device */
 	device_memory& rgba = rgba_data();
 	device->pixels_copy_from(rgba, 0, w, h);
 
-	uchar *in = (uchar*)rgba.data_pointer;
+	half *in = (half*)rgba.data_pointer;
 
 	int scw = params.width / w;
 	int sch = params.height / h;
@@ -460,7 +457,7 @@ void DisplayBuffer::get_pixels(Device *device, float* pixels)
 	for (int y = 0; y < draw_height; y++) {
 		for (int x = 0; x < draw_width; x++) {
 			tileidx = y * draw_width * 4 + x * 4;
-			f = make_float4(in[tileidx+0]/255.0f, in[tileidx+1]/255.0f, in[tileidx+2]/255.0f, in[tileidx+3]/255.0f);
+			f = make_float4(half_to_float(in[tileidx+0]), half_to_float(in[tileidx+1]), half_to_float(in[tileidx+2]), half_to_float(in[tileidx+3]));
 
 			/* scale up pixel if necessary, taking into account final image size*/
 			for (int target_y = y*sch; target_y < min(params.height, (y*sch+sch)); target_y++) {
